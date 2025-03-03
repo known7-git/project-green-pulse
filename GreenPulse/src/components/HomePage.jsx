77b4eac73959f29edb88e499e251b85fbe66fb31
Aman <amanasrj4545@gmail.com>
2025-03-03 14:50:59 +0530

    j

--- GreenPulse/src/components/HomePage.jsx
+++ GreenPulse/src/components/HomePage.jsx
@@ -7,11 +7,28 @@ import vid4 from '../assets/vid4.mp4';
 const HomePage = () => {
     return (
         <>
-            <div className="w-[80%] h-[80vh] justify-center place-self-center gap-1 mt-16 flex-row overflow-hidden flex">
-                <video src={vid1} autoPlay loop muted className='w-1/4 h-full video-rounded'></video>
-                <video src={vid2} autoPlay loop muted className='w-1/4 h-full video-rounded'></video>
-                <video src={vid3} autoPlay loop muted className='w-1/4 h-full video-rounded'></video>
-                <video src={vid4} autoPlay loop muted className='w-1/4 h-full video-rounded'></video>
+            <div className="w-[80%] h-full place-self-center gap-3 mt-16 flex-row flex scale-80">
+                <div className="video-container">
+                    <video src={vid1} autoPlay loop muted className='video'></video>
+                    <div className="overlay flex flex-col">
+                        <p className='text-left p-4 text-2xl motion-preset-slide-right motion-'>Total Deaths: 10,000</p>
+                        <p className='text-left p-4 text-2xl '>Total Injured: 50,000</p>
+                        <p className='text-left p-4 text-2xl '>Latest Earthquake: 2025-02-28</p>
+                        <p className='text-left p-4 text-2xl '>Next Prediction: 2025-03-15</p>
+                    </div>
+                </div>
+                <div className="video-container">
+                    <video src={vid2} autoPlay loop muted className='video'></video>
+                    <div className="overlay"></div>
+                </div>
+                <div className="video-container">
+                    <video src={vid3} autoPlay loop muted className='video'></video>
+                    <div className="overlay"></div>
+                </div>
+                <div className="video-container">
+                    <video src={vid4} autoPlay loop muted className='video'></video>
+                    <div className="overlay"></div>
+                </div>
             </div>
         </>
     );
